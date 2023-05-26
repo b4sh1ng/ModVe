d70859d42db334e466b011d10ad4d7a055794c0f
b4sh1ng <96059339+b4sh1ng@users.noreply.github.com>
2023-05-26 11:23:32 +0200

    Add files via upload

--- mysql.inc.php
+++ mysql.inc.php
@@ -1,5 +1,13 @@
 <?php
-define('MYSQL_HOST', 'localhost');
-define('MYSQL_USER', 'inf21');
-define('MYSQL_PW', 'test1');
-define('MYSQL_DB', 'ot');
+$db_host = "localhost";
+$db_user = "inf21";
+$db_pwd = "test1";
+$db_name = "ot";
+
+try {
+        $dsn = "mysql:host=$db_host;dbname=$db_name;";
+        $pdo = new PDO($dsn, $db_user, $db_pwd);
+    } catch (PDOException $er) {
+        echo "Failed: " . $er;
+    }
+?>
