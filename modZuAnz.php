dffa3f690dedb016772b696cd20aecc742668ac3
b4sh1ng <bashing@outlook.de>
2023-05-26 09:22:03 +0200

    added navbar

---
+++ modZuAnz.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modulzuordnung anzeigen | FST</title>
+</head>
+
+<body style="background-color:#1E90FF">
+    <?php require __DIR__ . "/global/navBar.html" ?>
+</body>
+
+</html>
--- schulBea.php
+++ schulBea.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Schüler anzeigen | FST</title>
+    <title>Schüler bearbeiten | FST</title>
 </head>
 
 <body style="background-color:#1E90FF">
