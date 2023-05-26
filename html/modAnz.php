f7c2c57128635abc9cd87cc00f0079e8a565f3d0
b4sh1ng <96059339+b4sh1ng@users.noreply.github.com>
2023-05-26 11:24:11 +0200

    Add files via upload
    Modul Anzeigen tabelle hinzugefügt

--- html/modAnz.php
+++ html/modAnz.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     <title>Modul anzeigen | FST</title>
 </head>
 
@@ -12,15 +14,37 @@
     <?php
     require __DIR__ . "/global/navBar.html"; //NavBar hinzufügen
     require_once "../mysql.inc.php";
-    try {
-        $dsn = "mysql:host=$db_host;dbname=$db_name;";
-        $pdo = new PDO($dsn, $db_user, $db_pwd);
-    } catch (PDOException $er) {
-        echo "Failed: " . $er;
-    }
-
-    $stmt = $pdo->prepare('S');
+    $stmt = $pdo->prepare('SELECT * FROM module');
+    $stmt->execute();
     ?>
+    <div>
+        <div style="margin: 25px; ">
+            <table class="table table-striped table-dark table-hover table-sm rounded">
+                <thead>
+                    <tr>
+                        <th>Modnr.</th>
+                        <th>Modul</th>
+                        <th>Modulstunden</th>
+                        <th>Lehrer ID</th>
+                        <th>Raum</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    foreach ($stmt as $result) {
+                        echo "<tr>
+                        <td>" . $result['Modnr'] . "</td>
+                        <td>" . $result['Modu'] . "</td>
+                        <td>" . $result['Mstd'] . "</td>
+                        <td>" . $result['Lid'] . "</td>
+                        <td>" . $result['Rnr'] . "</td>
+                        </tr>";
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </body>
 
 </html>
