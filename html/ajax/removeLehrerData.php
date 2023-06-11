9d4f3493379f60f1056d3756ce8ff885780af525
frank <fgraupeter@gmail.com>
2023-06-11 10:41:56 +0200

    lehrer kann nur gelöscht werden wenn keine module

--- html/ajax/removeLehrerData.php
+++ html/ajax/removeLehrerData.php
@@ -3,18 +3,19 @@ require_once "../../mysql.inc.php";
 
 $lid = $_POST['lid'];
 
-$sql = "DELETE FROM lehrer WHERE Lid=:lid";
-$stmt = $pdo->prepare($sql);
-$stmt->bindParam(':lid', $lid);
-$stmt->execute();
+$sqlCheck = "SELECT * from module WHERE Lid=:lid";
+$stmtCheck = $pdo->prepare($sqlCheck);
+$stmtCheck->bindParam(':lid', $lid);
+$stmtCheck->execute();
 
-// Handle the success or error response
-if ($stmt->rowCount() > 0) {
-    // Insert successful
-    $response = "Löschen Erfolgreich";
+if ($stmtCheck->rowCount() > 0) {
+    // There are rows in modulzurodnung table with the specified Snr
+    $response = "Löschen nicht möglich: Der Lehrer bietet ein oder mehrere Module an";
 } else {
-    // Insert failed
-    $response = "Löschen Fehlgeschlagen";
+    $sql = "DELETE FROM lehrer WHERE Lid=:lid";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':lid', $lid);
+    $stmt->execute();
+    $response = "Löschen Erfolgreich";
 }
 echo $response;
-
