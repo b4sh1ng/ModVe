edfb87fd3f2a550aa1a204de4f7fe336ed3787c4
b4sh1ng <96059339+b4sh1ng@users.noreply.github.com>
2023-06-11 20:23:20 +0200

    Update install_script.sh
    Standartskript zum einspielen der Datenbank

--- install_script.sh
+++ install_script.sh
@@ -1,1 +1,6 @@
+$ echo "Dies ist ein Installationsskript für ein Modulverwaltungssystem."
+$ read -p "Bitte ggib dein Myql Nutzernamen ein: " db_login
+$ read -ps "Bitte gib dein Mysql Passwort ein: " db_pass
+mysql --user=$db_login --password=$db_pass --verbose < base_database.sql
 
+$ echo "Datenbank wurde eingespielt!\nStandartnutzer mit Passwort wurde mit erstellt!\nDie Logindaten für diesen Nutzer sind aus der Dokumentation zu entnehmen."
