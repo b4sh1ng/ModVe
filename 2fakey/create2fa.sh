e5b074d7228d1c108a07ef9695436dff64f89d55
b4sh1ng <96059339+b4sh1ng@users.noreply.github.com>
2023-06-15 19:06:43 +0200

    Delete 2fakey directory

--- 2fakey/create2fa.sh
+++
@@ -1,7 +0,0 @@
-key=$(openssl rand -hex 12 | base32)
-
-qrencode -o ./2fakey/2fa_key.png "otpauth://totp/$1?secret=$key"
-mysql --user=$2 --password=$3 <<sql
-USE $4
-UPDATE verw SET 2fakey='$key' WHERE loginid='$1'
-sql
